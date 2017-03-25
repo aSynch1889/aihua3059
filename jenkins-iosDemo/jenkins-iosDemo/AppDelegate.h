806363274b3ab8416d74363dc38803ef3fe60fc4
sunny华子 <aizi1989010@126.com>
2017-03-25 14:58:42 +0800

    1、初始化本地项目

---
+++ jenkins-iosDemo/jenkins-iosDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  jenkins-iosDemo
+//
+//  Created by aaaa on 17/3/25.
+//  Copyright © 2017年 ISV Co.,Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ jenkins-iosDemo/jenkins-iosDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  jenkins-iosDemo
+//
+//  Created by aaaa on 17/3/25.
+//  Copyright © 2017年 ISV Co.,Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
